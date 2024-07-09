64724713cdba841a9d9f00915d4e896de667313c
lagab <contact.lagabservices@gmail.com>
2024-07-10 01:09:51 +0200

    init auth

--- database/migrations/1720532272670_create_users_table.ts
+++ database/migrations/1720532272670_create_users_table.ts
@@ -1,4 +1,4 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+import {BaseSchema} from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'users'
@@ -6,7 +6,8 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.string('first_name').nullable()
+      table.string('last_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
 
--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,27 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AuthController from "#controllers/auth_controller";
+import {middleware} from "#start/kernel";
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+
+router.group(() => {
+  router.post('/auth/register', [AuthController, 'register'])
+  router.post('/auth/login', [AuthController, 'login'])
+  router.post('/auth/logout', [AuthController, 'logout']).use(middleware.auth())
+  router.get('/', async () => {
+    return {
+      hello: 'world',
+    }
+  })
+  router.get('/me', async ({auth, response}) => {
+    try {
+      const user = auth.getUserOrFail()
+      return response.ok(user)
+    } catch (error) {
+      return response.unauthorized({error: 'User not found'})
+    }
+  })
+    .use(middleware.auth())
+}).prefix('/api/v1')
+
